46bb201d556f6e5ffda9c7e74aa6ce26fa4c9e05
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2021-03-12 17:59:04 +0000

    Fix condition of who can choose to have the post pg'd

--- peerforum/classes/post_form.php
+++ peerforum/classes/post_form.php
@@ -168,9 +168,7 @@ class mod_peerforum_post_form extends moodleform {
             }
 
             if (empty($post->id) && $post->parent && $peerforum->peergradeassessed) {
-                $pgm = \mod_peerforum\local\container::get_manager_factory()->get_peergrade_manager();
-                $pgpermissions = $pgm->get_plugin_permissions_array($modcontext->id, 'mod_peerforum', 'post');
-                if ($pgm->check_peergrade_permission($pgpermissions, $peerforum->finalgrademode)) {
+                if ($peerforum->gradeprofessorpost || !has_capability('mod/peerforum:professorpeergrade', $modcontext)) {
                     $mform->addElement('checkbox', 'nopeergrade', 'Don\'t peer grade this post');
                     $mform->setDefault('nopeergrade', 0);
                 }
